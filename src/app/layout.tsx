7c01d2fa089cce9e2f23465f53866e227d5a6757
ImHyunJeong <fltxld3@naver.com>
2023-11-24 15:56:30 +0900

    feat: Add myLogo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React, { useEffect } from 'react';
 import './globals.css';
 import GlobalStyle from './styles/global-styles';
@@ -7,6 +6,18 @@ import Header from './_components/header';
 import useHasMounted from '../useHasMounted';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { Metadata } from 'next';
+
+//export const metadata: Metadata = {
+// title: {
+// default: '임현정 블로그',
+//template: '임현정 블로그 ',
+//},
+//description: '프론트엔드 개발자의 블로그',
+// icons: {
+//  icon: '/myLogo.ico',
+//},
+//};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const hasMounted = useHasMounted();
@@ -18,14 +29,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
-      <head></head>
+      <head>{/* <link rel="icon" href="/myLogo.ico" sizes="any" />*/}</head>
 
       <body>
         {hasMounted && (
           <ThemeProvider theme={theme}>
             <GlobalStyle />
             <Header />
-            {children}
+            <main>{children}</main>
           </ThemeProvider>
         )}
       </body>
